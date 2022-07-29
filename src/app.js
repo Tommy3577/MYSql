fd2434af804fe554b5035626425dbc2c40259cd3
Tommy3577 <101331469+Tommy3577@users.noreply.github.com>
2022-07-29 13:43:26 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const { sequelize } = require("./db/connection");
-const { createMovie, readMovie, updateMovie, deleteMovie } = require("./movie/functions");
+const { createMovie, readMovie, updateMovie, deleteMovie, findMovie } = require("./movie/functions");
 
 const app = async (yargsObj) => {
   await sequelize.sync({ alter: true });
@@ -15,11 +15,16 @@ const app = async (yargsObj) => {
     //list movies from db
   }
 //----------------------------------------------------------------------------------------------------
+else if (yargsObj.find) {
+  await findMovie({ title: yargsObj.title, actor: yargsObj.actor});
+  //find a specific movie or actor
+}
+//----------------------------------------------------------------------------------------------------
   else if (yargsObj.update) {
     await updateMovie({ title: yargsObj.title, newTitle: yargsObj.newTitle, actor: yargsObj.actor, newActor: yargsObj.newActor });
     //update a movie from db
   }
-//------------------------------------------------------------------------------------  ----------------
+//----------------------------------------------------------------------------------------------------
   else if (yargsObj.delete) {
     await deleteMovie({where:{title: yargsObj.title}});
     //delete a movie from db
@@ -29,10 +34,5 @@ const app = async (yargsObj) => {
     console.log("Incorrect command");
   }
 };
-
+//----------------------------------------------------------------------------------------------------
 app(yargs.argv);
-
-
-
-
-// https://sequelize.org/docs/v6/core-concepts/model-querying-basics/
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -19,14 +19,23 @@ exports.readMovie = async (movieObj) => {
 }
 //----------------------------------------------------------------------------------------------------
 exports.updateMovie = async (movieObj) => {
-    try {
-        // await Movie.update({title: movieObj.newTitle},{where:{title: movieObj.title}});
-
-        await Movie.update({title: movieObj.newTitle, actor: movieObj.newActor}, {where:{title: movieObj.title, actor: movieObj.actor}});
-    } catch (error){
-        console.log(error)
-    }
-}
+    try{
+        if (movieObj.title) {
+          await Movie.update({title: movieObj.newTitle}, {where:{title: movieObj.title}});
+          console.log(`Title ${movieObj.title} updated to ${movieObj.newTitle}`)
+        }
+        else if (movieObj.actor) {
+          await Movie.update({actor: movieObj.newActor}, {where:{actor: movieObj.actor}});
+          console.log(`Actor ${movieObj.actor} updated to ${movieObj.newActor}`)
+        }
+        else {
+          console.log("Invalid command")
+        }
+      }
+      catch(error) {
+        console.log(error);
+      }
+    };
 //----------------------------------------------------------------------------------------------------
 exports.deleteMovie = async (movieObj) => {
     try {
@@ -35,3 +44,22 @@ exports.deleteMovie = async (movieObj) => {
         console.log(error)
     }
 }
+//----------------------------------------------------------------------------------------------------
+exports.findMovie = async (movieObj) => {
+    try{
+      if (movieObj.title) {
+        const results = await Movie.findAll({where:{title: movieObj.title}});
+        console.log(results);
+      }
+      else if (movieObj.actor) {
+        const results = await Movie.findAll({where:{actor:movieObj.actor}});
+        console.log(results);
+      }
+      else {
+        console.log("Invalid command")
+      }
+    }
+    catch(error) {
+      console.log(error);
+    }
+  };
